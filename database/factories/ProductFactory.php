24d559e618513f9508d1e7fa8c03d98f165b995b
Yonathan2006 <yonathanmorgan18@gmail.com>
2025-01-28 15:05:04 -0500

    Created model, factory, seeder, and database migration for `Product` resource.

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word(),
+            'description' => $this->faker->sentence(),
+            'status' => $this->faker->boolean(70),
+            'user_id' => User::all()->random()->id,
+        ];
+    }
+}
