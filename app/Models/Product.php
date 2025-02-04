8672d6caa8bf9c5ea09ac3437c11ef67952f812d
jairomcr <jairocascaretrodriguez@gmail.com>
2025-02-04 10:10:08 -0500

    removing elements from the home that are not needed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class Product extends Model
 {
@@ -13,11 +13,13 @@ class Product extends Model
     use HasFactory;
 
     // one-to-one relationship
-    public function user_id(): BelongsTo {
+    public function user_id(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function image_id(): HasOne {
-        return $this->hasOne(Image::class, 'image_id', 'imageable_id');
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
     }
 }
--- resources/views/front-end/posts/layouts/footer.blade.php
+++ resources/views/front-end/posts/layouts/footer.blade.php
@@ -1,9 +1,7 @@
 <footer id="footer">
-
     <div class="footer-top">
         <div class="container">
             <div class="row">
-
                 <div class="col-lg-3 col-md-6 footer-contact">
                     <h3>Flexor</h3>
                     <p>
