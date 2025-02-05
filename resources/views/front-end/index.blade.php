166d8825189e9cfa7f154a4155660e3a8fdd2a3f
Yony2006 <yonathanmorgan18@gmail.com>
2025-02-05 01:47:22 -0500

    Modified view (front-end.index): - Moved the "main.js" script dependence declaration so it's loded AFTER its dependences

--- resources/views/front-end/index.blade.php
+++ resources/views/front-end/index.blade.php
@@ -309,9 +309,7 @@
 
 
 
-    <!-- Template Main JS File -->
-    <script src="assets/js/main.js"></script>
-    @stack('scripts')
+
 
     <!-- Vendor JS Files -->
     <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
@@ -320,6 +318,10 @@
     <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+    <!-- Template Main JS File -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    @stack('scripts')
+
 </body>
 
 </html>
